Bandit notebook variance fix, nonstationary preps

action.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-class Action:
+class StationaryAction:
     def __init__(self, mean: float, std_dev: float = 1.0) -> None:
         self.mean_reward = mean
         self.std_dev = std_dev
@@ -9,3 +9,6 @@ class Action:
         return np.random.normal(self.mean_reward, self.std_dev)
 
     
+class NonStationaryAction:
+    def apply(self) -> float:
+        return np.random.normal(np.random.randn(), np.random.randn())